20dabbb579050e33f0bbdc16f05ff92dc548e792
Yoshinari Nomura <nom@quickhack.net>
2024-12-12 21:42:16 +0900

    ibundle gem tapocon
    bundle gem tapocon --no-coc -t
    bundle config set path vendor/bundle
    bundle install
    
    get .gitignore from
      https://raw.github.com/github/gitignore/master/Ruby.gitignore

---
+++ lib/tapocon.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tapocon/version"
+
+module Tapocon
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tapocon/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tapocon
+  VERSION = "0.1.0"
+end
